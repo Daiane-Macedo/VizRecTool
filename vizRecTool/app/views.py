ed050a719956f910dcdee89b05ff8500e6141305
Daiane-Macedo <daianemacedo@id.uff.br>
2019-09-10 20:14:56 -0300

    iniciando interface

---
+++ vizRecTool/app/views.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from django.views.generic import TemplateView
+from django.shortcuts import render
+import altair as alt
+import pandas as pd
+from vega_datasets import data
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get(self, request, *args, **kwargs):
+        context = locals()
+
+        data3 = pd.DataFrame({
+            'Eixo x': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
+            'Eixo y': [28, 55, 43, 91, 81, 53, 19, 87, 52]
+        })
+
+        context['chart'] = alt.Chart(data3).mark_bar().encode(
+            x='Eixo x',
+            y='Eixo y'
+        ).interactive()
+
+        source = data.cars()
+        # print(data.cars());
+        context['chart2'] = alt.Chart(source).mark_circle().encode(
+            x='Horsepower',
+            y='Miles_per_Gallon',
+            color='Origin'
+        ).interactive()
+
+        return render(request, self.template_name, context)
