98b1ac800fe6e32f8e4862a8067a20aeb6882e6c
Daiane-Macedo <daianemacedo@id.uff.br>
2019-09-12 01:57:47 -0300

    dando continuidade ao front-end

---
+++ vizRecTool/app/static/js/index.js
@@ -0,0 +1,8 @@
+$('.new_Btn').bind("click" , function () {
+        $('#html_btn').click();
+    });
+/*
+  <div class="file-upload mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                    <span>BROWSE</span>
+                    <input type="file" name="FileAttachment" id="FileAttachment" class="upload" />
+                </div>{% endcomment %}*/
