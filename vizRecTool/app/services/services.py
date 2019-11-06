9462c7342f1810c1741906037aac6634031ab382
Daiane-Macedo <daianemacedo@id.uff.br>
2019-11-06 01:34:31 -0300

    ajuste gráfico de colunas

--- vizRecTool/app/services/services.py
+++ vizRecTool/app/services/services.py
@@ -68,13 +68,18 @@ class Chart:
 
         uniqueVal = df[xAxis].unique()
         if xAxis in col.categorical and yAxis in col.quantitative and len(uniqueVal) < 3:  # pie
-            chartsList = [build_pie_chart(df, xAxis, yAxis, fileName)]
-
+            chartsList = []
+            pie_chart = build_pie_chart(df, xAxis, yAxis, fileName)
+            chartsList.append(pie_chart)
+            bar_chart = build_bar_chart(df, xAxis, yAxis, fileName)
+            chartsList.append(bar_chart)
             # def generate_tumbnail_images
             return chartsList
 
         if xAxis in col.categorical and yAxis in col.quantitative:  # bar
-            chartsList = build_bar_chart(df, xAxis, yAxis, fileName)
+            chartsList = []
+            bar_chart = build_bar_chart(df, xAxis, yAxis, fileName)
+            chartsList.append(bar_chart)
             return chartsList
 
         if xAxis in col.date and yAxis in col.quantitative:  # line
@@ -93,7 +98,9 @@ class Chart:
             return chartsList
 
         if xAxis in col.quantitative and yAxis in col.quantitative:  # scatter plot
-            chartsList = build_scatter_plot(df, xAxis, yAxis, fileName)
+            chartsList = []
+            chart = build_scatter_plot(df, xAxis, yAxis, fileName)
+            chartsList.append(chart)
             return chartsList
 
 
@@ -131,25 +138,30 @@ def build_line_chart(dataframe, category, xAxis, yAxis, chartName):
 
     chart = Chart()
     chart.content = opy.plot(figure, auto_open=False, output_type='div')
-
     return chart
 
 
-def build_bar_chart(df, xAxis, yAxis, chartName):
-    chartsList = []
-    data = [go.Bar(
-        x=df[xAxis],
-        y=df[yAxis],
-        showlegend=True, name=yAxis
-    )]
+def build_bar_chart(dataframe, xAxis, yAxis, chartName):
+    trace = go.Bar(showlegend=True)
     layout = getLayout(xAxis, yAxis, chartName)
-    figure = go.Figure(data=data, layout=layout)
+    figure = go.Figure(data=trace, layout=layout)
+    dataframe[yAxis] = pd.to_numeric(dataframe[yAxis], errors='coerce')  # convert to numeric
+    df = dataframe[[yAxis, xAxis]]
+
+    # add traces to chart
+    unique = df[xAxis].unique()
+    df = dataframe.groupby([xAxis], as_index=False)[yAxis].sum()
+    for name in unique:
+        df2 = dict(tuple(df.groupby(xAxis)))
+        df2 = df2[name]
+        trace.name = name
+        trace.y = df2[yAxis]
+        trace.x = df2[xAxis]
+        figure.add_trace(trace)
+
     chart = Chart()
     chart.content = opy.plot(figure, auto_open=False, output_type='div')
-    chartsList.append(chart)
-    # truncate list in 5 charts (max)
-    chartsList = chartsList[0: 5]
-    return chartsList
+    return chart
 
 
 def build_pie_chart(df, labels, values, fileName):
@@ -159,13 +171,14 @@ def build_pie_chart(df, labels, values, fileName):
         showlegend=True, name=labels
     )]
     figure = go.Figure(data)
+    figure.update_layout(title_text=fileName)
     chart = Chart()
     chart.content = opy.plot(figure, auto_open=False, output_type='div')
+
     return chart
 
 
 def build_scatter_plot(df, xAxis, yAxis, chartName):
-    chartsList = []
     trace1 = go.Scatter(x=df[xAxis], y=df[yAxis],
                         marker=dict(symbol='circle'),
                         mode='markers',
@@ -175,9 +188,8 @@ def build_scatter_plot(df, xAxis, yAxis, chartName):
     figure = go.Figure(data=data, layout=layout)
     chart = Chart()
     chart.content = opy.plot(figure, auto_open=False, output_type='div')
-    chartsList.append(chart)
 
-    return chartsList
+    return chart
 
 
 def categorize_columns(line, header):
