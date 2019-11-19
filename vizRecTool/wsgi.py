768931db625e938c5f9bf2678ccfdf7ff9e43396
Daiane-Macedo <daianemacedo@id.uff.br>
2019-11-18 23:06:09 -0300

    Deploy da aplicação

--- vizRecTool/wsgi.py
+++ vizRecTool/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "altairproject.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vizRecTool.settings")
 
 application = get_wsgi_application()
